2d8213c6a7e42921c819f47b6fe83da5c64ea771
Asher <ashish.2017@hotmail.com>
2023-11-13 09:09:13 +0000

    basic server

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,46 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, conway-gol-wasm!");
+#[repr(u8)] // represent each cel as a single byte
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum GameCell {
+    Dead = 0, // simplifies cell's live neighbour counting with addition
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<GameCell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {name}!"));
 }
